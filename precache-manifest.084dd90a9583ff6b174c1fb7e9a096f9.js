969accc279eef3e35f94f8bb23e2d5e2ebbc3344
HossamMuhammedOmar <hossamomar9876@gmail.com>
2020-08-24 21:45:29 +0200

    Updates

---
+++ precache-manifest.084dd90a9583ff6b174c1fb7e9a096f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c1171760e52147a5e5e58145524b274",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "b747e1d762d09f49a79b",
+    "url": "/pokedex/static/css/main.f1149d62.chunk.css"
+  },
+  {
+    "revision": "0fd19ba77dbbbad912ad",
+    "url": "/pokedex/static/js/2.0f976a46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.0f976a46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b747e1d762d09f49a79b",
+    "url": "/pokedex/static/js/main.32c9591f.chunk.js"
+  },
+  {
+    "revision": "f3dfa4f78aa1b7c54f4d",
+    "url": "/pokedex/static/js/runtime-main.0a8f8a7c.js"
+  }
+]);
